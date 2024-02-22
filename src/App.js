c3e1ac164f2e9963800fc9dd02eb20ee052be57f
David <dakinola54@gmail.com>
2024-02-21 19:30:12 -0800

    base files and folders

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import Nav from "./components/Nav";
 
 function App() {
  
   return (
     <div className="App">
-      
+      <Nav />
     </div>
   );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,17 @@
+import './Navbar.css'
+
+const Nav = () => {
+    return(
+        <div className="navbar">
+            <nav>
+                <h1>InkHive</h1>
+                <ul>
+                    <li>Home</li>
+                    <li>New Blog</li>
+                </ul>
+            </nav>
+        </div>
+    );
+};
+
+export default Nav;
