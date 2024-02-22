071431bfbd388b178a5f59e5640d4d9133b45705
David <dakinola54@gmail.com>
2024-02-22 13:14:41 -0800

    using other ways for event

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,11 +2,13 @@ import './Home.css';
 const Home = () => {
 
     const handleClick = () => console.log('Clicked');
+    const handleClickAgain = name => console.log('Hello ' + name)
 
     return(
         <div className="home">
             <h1>Homepage</h1>
             <button onClick={handleClick}>Click this</button>
+            <button onClick={() => handleClickAgain(prompt('What is your name?'))}>Click again</button>
         </div>
     );
 };
